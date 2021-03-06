1054c9a59ed4e25f2d738327f1df2c518583f94f
sanix-sandel <36574404+sanix-sandel@users.noreply.github.com>
2021-03-06 03:53:32 +0300

    zuul service

---
+++ zuulapp/src/test/java/com/example/zuulapp/ZuulappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zuulapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
